ee8602aefb4a0eca560010aa747b4c4a7653274f
alvaroescoto <alvaroedeavila@gmail.com>
2025-07-06 19:49:58 +0200

    Fix test environment for DOM access using jsdom

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
+  preset: "ts-jest",
+  testEnvironment: "jsdom", // ✅ Use jsdom to support DOM APIs like document
+  testMatch: ["**/tests/**/*.test.ts"],
   globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.jest.json' // 👈 Use this config only for tests
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json"
     }
   }
 };
